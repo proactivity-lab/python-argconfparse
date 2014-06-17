750d448c595e71a8d38d455a43c7c2eb0898dc16
Raido Pahtma <raidopahtma@gmail.com>
2014-06-18 00:26:54 +0300

    Added some functions for different argument types.

--- argconfparse/argconfparse.py
+++ argconfparse/argconfparse.py
@@ -8,9 +8,7 @@ import ConfigParser
 
 
 class ConfigArgumentParser(object):
-    """
-    Argumentparser that can take options from a configuration file.
-    """
+    """ Argument parser that can take options from a configuration file. """
 
     def __init__(self, section, **kwargs):
         """
@@ -20,11 +18,15 @@ class ConfigArgumentParser(object):
         self.section = section
 
         self.conf_parser = argparse.ArgumentParser(add_help=False)
-        self.conf_parser.add_argument("-c", "--conf_file", help="Specify config file", metavar="FILE")
+        self.conf_parser.add_argument("-c", "--conf-file", help="Specify config file", metavar="FILE")
 
         self.parser = argparse.ArgumentParser(parents=[self.conf_parser], **kwargs)
 
     def parse_args(self):
+        """
+        Parse command-line and configuration file arguments.
+        :return: Parsed arguments.
+        """
         args, remaining_argv = self.conf_parser.parse_known_args()
 
         if args.conf_file:
@@ -49,6 +51,27 @@ class ConfigArgumentParser(object):
 
     def add_argument(self, *args, **kwargs):
         self.parser.add_argument(*args, **kwargs)
+
+
+def arg_str2bool(v):
+    """ Use this for boolean options, regular bool is always treated as True. """
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+
+def arg_hex2int(v):
+    """ Parse integers that can be written in decimal or hex when written with 0xXXXX. """
+    return int(v, 0)
+
+
+def arg_check_hex16str(v):
+    """ Verify that the provided argument is a 16 character hexadecimal string """
+    if isinstance(v, str):
+        if len(v) == 16:
+            v.decode("hex")
+            return v
+        else:
+            raise ValueError("Value must be 16 characters long")
+    raise ValueError("Value must be a string")
 
 
 if __name__ == '__main__':

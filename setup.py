b222cd828afae0f2075fc8aa399045dd556ba764
Raido Pahtma <raidopahtma@gmail.com>
2016-01-31 22:03:43 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='argconfparse',
+      version='0.1.0',
+      description='Python argparse with options to read argument values from configuration files.',
+      url='http://github.com/proactivity-lab/python-argconfparse',
+      author='Raido Pahtma',
+      author_email='raido.pahtma@ttu.ee',
+      license='MIT',
+      packages=['argconfparse'],
+      zip_safe=False)
